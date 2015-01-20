a1606060349c68c244cc6b95b31d88123d2e7acd
Wiliam Souza <wiliamsouza83@gmail.com>
2015-01-20 00:16:24 -0200

    Added empty_buckets_fill_in test

--- hystrix/rolling_number.py
+++ hystrix/rolling_number.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from multiprocessing import Value, Lock
 from collections import deque
 from enum import Enum
+import time
 import logging
 
 log = logging.getLogger(__name__)
@@ -24,12 +25,13 @@ class RollingNumber(object):
     each 10 second window starting at 0 again.
     '''
 
-    def __init__(self, time, milliseconds, bucket_numbers):
-        self.time = time
+    def __init__(self, _time, milliseconds, bucket_numbers):
+        self.time = _time
         self.milliseconds = milliseconds
         self.buckets = BucketCircular(bucket_numbers)
         self.bucket_numbers = bucket_numbers
         self.cumulative_sum = CumulativeSum()
+        self._new_bucket_lock = Lock()
 
         if self.milliseconds % self.bucket_numbers != 0:
             raise Exception('The milliseconds must divide equally into '
@@ -49,26 +51,38 @@ class RollingNumber(object):
         if current_bucket is not None and current_time < current_bucket.window_start + self.time.current_time_in_millis():
             return current_bucket
 
-        # If we didn't find the current bucket above, then we have to
-        # create one.
-        if not self.buckets.peek_last():
-            new_bucket = Bucket(current_time)
-            self.buckets.add_last(new_bucket)
-            return new_bucket
-        else:
-            for i in range(self.bucket_numbers):
-                last_bucket = self.buckets.peek_last()
-                time = last_bucket.window_start + self.buckets_size_in_milliseconds()
-                if current_time < time:
-                    return last_bucket
-                elif current_time - time > self.milliseconds:
-                    self.reset()
-                    return self.current_bucket()
-                else:
-                    self.buckets.add_last(Bucket(time))
-                    self.cumulative_sum.add_bucket(last_bucket)
+        with self._new_bucket_lock:
+            # If we didn't find the current bucket above, then we have to
+            # create one.
+            if self.buckets.peek_last() is None:
+                new_bucket = Bucket(current_time)
+                self.buckets.add_last(new_bucket)
+                return new_bucket
+            else:
+                for i in range(self.bucket_numbers):
+                    last_bucket = self.buckets.peek_last()
+                    if current_time < last_bucket.window_start + self.buckets_size_in_milliseconds():
+                        return last_bucket
+                    elif current_time - (last_bucket.window_start + self.buckets_size_in_milliseconds()) > self.milliseconds:
+                        self.reset()
+                        return self.current_bucket()
+                    else:
+                        self.buckets.add_last(Bucket(last_bucket.window_start + self.buckets_size_in_milliseconds()))
+                        self.cumulative_sum.add_bucket(last_bucket)
 
-            return self.buckets.peek_last()
+                return self.buckets.peek_last()
+
+        # we didn't get the lock so just return the latest bucket while
+        # another thread creates the next one
+        current_bucket = self.buckets.peek_last()
+        if current_bucket is not None:
+            return current_bucket
+        else:
+            # The rare scenario where multiple threads raced to create the
+            # very first bucket wait slightly and then use recursion while
+            # the other thread finishes creating a bucket
+            time.sleep(5)
+            self.current_bucket()
 
     def reset(self):
         last_bucket = self.buckets.peek_last()
@@ -93,7 +107,7 @@ class BucketCircular(deque):
 
     def peek_last(self):
         try:
-            return self[-1]
+            return self[0]
         except IndexError:
             return None
 
@@ -112,11 +126,11 @@ class Bucket(object):
         # TODO: Change this to use a metaclass
         for name, event in RollingNumberEvent.__members__.items():
             if event.is_counter():
-                self._adder[name] = LongAdder()
-                continue
+                self._adder[event.name] = LongAdder()
 
+        for name, event in RollingNumberEvent.__members__.items():
             if event.is_max_updater():
-                self._max_updater[name] = LongMaxUpdater()
+                self._max_updater[event.name] = LongMaxUpdater()
 
     def get(self, event):
         if event.is_counter():
@@ -185,11 +199,11 @@ class CumulativeSum(object):
         # TODO: Change this to use a metaclass
         for name, event in RollingNumberEvent.__members__.items():
             if event.is_counter():
-                self._adder[name] = LongAdder()
-                continue
+                self._adder[event.name] = LongAdder()
 
+        for name, event in RollingNumberEvent.__members__.items():
             if event.is_max_updater():
-                self._max_updater[name] = LongMaxUpdater()
+                self._max_updater[event.name] = LongMaxUpdater()
 
     def add_bucket(self, bucket):
         for name, event in RollingNumberEvent.__members__.items():
--- tests/test_rolling_number.py
+++ tests/test_rolling_number.py
@@ -1,3 +1,5 @@
+from multiprocessing import Value, Lock
+
 from hystrix.rolling_number import RollingNumber, RollingNumberEvent
 
 import pytest
@@ -7,26 +9,73 @@ def test_create_buckets():
     time = MockedTime()
     counter = RollingNumber(time, 200, 10)
 
-    #  confirm the initial settings
+    # confirm the initial settings
     assert counter.milliseconds == 200
     assert counter.bucket_numbers == 10
     assert counter.buckets_size_in_milliseconds() == 20
 
-    #  we start out with 0 buckets in the queue
+    # We start out with 0 buckets in the queue
     assert counter.buckets.size == 0
 
-    #  add a success in each interval which should result in all 10 buckets
-    #  being created with 1 success in each
+    # add a success in each interval which should result in all 10 buckets
+    # being created with 1 success in each
     for r in range(counter.bucket_numbers):
+        import ipdb; ipdb.set_trace()  # Breakpoint
         counter.increment(RollingNumberEvent.SUCCESS)
         time.increment(counter.buckets_size_in_milliseconds())
 
-    #  confirm we have all 10 buckets
+    # confirm we have all 10 buckets
     assert counter.buckets.size == 10
 
-    #  add 1 more and we should still only have 10 buckets since that's the max
+    # add 1 more and we should still only have 10 buckets since that's the max
     counter.increment(RollingNumberEvent.SUCCESS)
     assert counter.buckets.size == 10
+
+
+def test_reset_buckets():
+    time = MockedTime()
+    counter = RollingNumber(time, 200, 10)
+
+    # We start out with 0 buckets in the queue
+    assert counter.buckets.size == 0
+
+    # Add 1
+    counter.increment(RollingNumberEvent.SUCCESS)
+
+    # Confirm we have 1 bucket
+    assert counter.buckets.size == 1
+
+    # Confirm we still have 1 bucket
+    assert counter.buckets.size == 1
+
+    # Add 1
+    counter.increment(RollingNumberEvent.SUCCESS)
+
+    # We should now have a single bucket with no values in it instead of 2 or
+    # more buckets
+    assert counter.buckets.size == 1
+
+
+def test_empty_buckets_fill_in():
+    time = MockedTime()
+    counter = RollingNumber(time, 200, 10)
+
+    # We start out with 0 buckets in the queue
+    assert counter.buckets.size == 0
+
+    # Add 1
+    counter.increment(RollingNumberEvent.SUCCESS)
+
+    # Confirm we have 1 bucket
+    assert counter.buckets.size == 1
+
+    time.increment(counter.buckets_size_in_milliseconds() * 3)
+
+    # Add another
+    counter.increment(RollingNumberEvent.SUCCESS)
+
+    # We should have 4 (1 + 2 empty + 1 new one) buckets
+    assert counter.buckets.size == 4
 
 
 def test_milliseconds_buckets_size_error():
@@ -49,11 +98,14 @@ def test_rolling_number_event_is_max_updater():
 class MockedTime():
 
     def __init__(self):
-        self.time = 0
+        self._time = Value('f', 0)
+        self._lock = Lock()
 
     def current_time_in_millis(self):
-        return self.time
+        with self._lock:
+            return self._time.value
 
     def increment(self, millis):
-        self.time += millis
-        return self.time
+        with self._lock:
+            self._time.value += millis
+        return self.current_time_in_millis()
