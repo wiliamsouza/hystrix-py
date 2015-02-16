c6c6c3e34cfbfb02a95b175afea72f5c37248baa
Wiliam Souza <wiliamsouza83@gmail.com>
2015-02-16 20:38:17 -0200

    Removed unused hack and added parent folder to path

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,11 @@ import os
 import sphinx_rtd_theme
 
 
-# Hack to fix six.with_metaclass bug with autodoc
-#from hystrix import rolling_number as rn
-#rn.RollingNumberEvent.__base__ = rn.Event.__base__
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
