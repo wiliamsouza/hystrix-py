d7dacb45104d2e0f66a3e34844d7531340630e81
Wiliam Souza <wiliamsouza83@gmail.com>
2015-02-15 22:19:26 -0200

    Added HealthCounts and Metrics class

--- hystrix/metrics.py
+++ hystrix/metrics.py
@@ -1,39 +1,103 @@
-''' Used by :class:`hystrix.command.HystrixCommand` to record metrics.'''
+''' Used by :class:`hystrix.command.Command` to record metrics.'''
 from __future__ import absolute_import
 import logging
 
 import six
 
+from hystrix.rolling_number import RollingNumber
+
 log = logging.getLogger(__name__)
 
 
+class Metrics(object):
+    """ Base class for metrics
+
+    Args:
+        counter (:class:`hystrix.rolling_number.RollingNumber`): Used to
+            increment or set values over time.
+    """
+
+    def __init__(self, counter):
+        self.counter = counter
+
+    def cumulative_count(self, event):
+        """ Cumulative count
+
+        Get the **cumulative** count since the start of the application for the
+        given :class:`RollingNumberEvent`.
+
+        Args:
+            event (:class:`RollingNumberEvent`): The Event to retrieve a
+                **sum** for.
+
+        Returns:
+            long: Returns the long cumulative count.
+        """
+        return self.counter.cumulative_sum(event)
+
+    def rolling_count(self, event):
+        """ **Rolling** count
+
+        Get the rolling count for the given:class:`RollingNumberEvent`.
+
+        Args:
+            event (:class:`RollingNumberEvent`): The Event to retrieve a
+                **sum** for.
+
+        Returns:
+            long: Returns the long cumulative count.
+        """
+        return self.counter.rolling_sum(event)
+
+
 class CommandMetricsMetaclass(type):
+    """ Metaclass for :class:`CommandMetrics`
+
+    Return a cached or create the :class:`CommandMetrics` instance for a given
+    :class:`hystrix.command.Command` name.
+
+    This ensures only 1 :class:`CommandMetrics` instance per
+    :class:`hystrix.command.Command` name.
+    """
 
     __instances__ = dict()
     __blacklist__ = ('CommandMetrics', 'CommandMetricsMetaclass')
 
     def __new__(cls, name, bases, attrs):
 
+        # Do not use cache for black listed classes.
         if name in cls.__blacklist__:
             return super(CommandMetricsMetaclass, cls).__new__(cls, name,
                                                                bases, attrs)
 
+        # User defined class name or create a default.
         class_name = attrs.get('__command_metrics_name__',
                                '{}CommandMetrics'.format(name))
-        new_class = super(CommandMetricsMetaclass, cls).__new__(cls,
-                                                                class_name,
-                                                                bases, attrs)
-        setattr(new_class, 'command_metrics_name', class_name)
 
+        # Check for CommandMetrics class instance
         if class_name not in cls.__instances__:
+            new_class = super(CommandMetricsMetaclass, cls).__new__(cls,
+                                                                    class_name,
+                                                                    bases,
+                                                                    attrs)
+            setattr(new_class, 'command_metrics_name', class_name)
             cls.__instances__[class_name] = new_class
 
         return cls.__instances__[class_name]
 
 
-class CommandMetrics(six.with_metaclass(CommandMetricsMetaclass, object)):
-
+class CommandMetrics(six.with_metaclass(CommandMetricsMetaclass, Metrics)):
+    """ Command metrics
+    """
     __command_metrics_name__ = None
+
+    # def __init__(self, command_key, command_group, properties,
+    #             event_notifier):
+    def __init__(self):
+        # self.properties = properties
+        # counter = RollingNumber(self.properties.milliseconds,
+        #                        self.properties.bucket_numbers)
+        super(CommandMetrics, self).__init__(None)  # use counter here
 
 
 class ExecutorMetricsMetaclass(type):
@@ -63,3 +127,41 @@ class ExecutorMetricsMetaclass(type):
 class ExecutorMetrics(six.with_metaclass(ExecutorMetricsMetaclass, object)):
 
     __executor_metrics_name__ = None
+
+
+class HealthCounts(object):
+    """ Number of requests during rolling window.
+
+    Number that failed (failure + success + timeout + thread pool rejected +
+    short circuited + semaphore rejected).
+
+    Error percentage;
+    """
+    def __init__(self, total, error, error_percentage):
+        self._total_count = total
+        self._error_count = error
+        self._error_percentage = error_percentage
+
+    def total_requests(self):
+        """ Total reqeust
+
+        Returns:
+            int: Returns total request count.
+        """
+        return self._total_count
+
+    def error_count(self):
+        """ Error count
+
+        Returns:
+            int: Returns error count.
+        """
+        return self._error_count
+
+    def error_percentage(self):
+        """ Error percentage
+
+        Returns:
+            int: Returns error percentage.
+        """
+        return self._error_percentage
